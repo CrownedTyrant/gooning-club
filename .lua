99676bf49dbb24eccaff77edad98f7c43793230f
idkkdiasdf <doesitevenmatter326@gmail.com>
2025-03-23 15:06:45 -0500

    Update .lua

--- .lua
+++ .lua
@@ -1,22 +1,3 @@
 --require(100263845596551)("Minos_Prime1PRank", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
 
 game.ServerStorage.AdminUi:Clone().Parent = game.Players.Minos_Prime1PRank.PlayerGui
-
---require(5227036455).rk("Minos_Prime1PRank")
---[[game.Players:BanAsync(
-{
-
-        UserIds = {2991435158},
-
-        Duration = 1,
-
-        DisplayReason = "death upon thee.",
-
-        PrivateReason = "said a list of slurs so horrible not even osama was proud",
-
-        ExcludeAltAccounts = true,
-
-        ApplyToUniverse = true
-
-    }
-)]]
