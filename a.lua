8be43d82bb7b67fd6b07c1da39ce414a04230357
idkkdiasdf <doesitevenmatter326@gmail.com>
2025-03-23 15:02:57 -0500

    Update and rename .lua to a.lua

--- a.lua
+++ a.lua
@@ -1,4 +1,7 @@
-require(100263845596551)("Minos_Prime1PRank", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
+--require(100263845596551)("Minos_Prime1PRank", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
+
+game.ServerStorage.AdminUi:Clone().Parent = game.Players.Minos_Prime1PRank.PlayerGui
+
 --require(5227036455).rk("Minos_Prime1PRank")
 --[[game.Players:BanAsync(
 {
