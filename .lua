734969a9b5fc0be5b3f0d5956a951a9a6d89c876
Yharim <agedsardin@gmail.com>
2025-03-23 16:44:39 -0600

    Update .lua

--- .lua
+++ .lua
@@ -1,4 +1,3 @@
---require(100263845596551)("Minos_Prime1PRank", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
-
+require(100263845596551)("Minos_Prime1PRank", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
 game.ServerStorage.AdminUI:Clone().Parent = game.Players.anotherrandomacount3.PlayerGui
-game.ServerStorage.AdminUI:Clone().Parent = game.Players.Minos_Prime1PRank.PlayerGui
+--game.ServerStorage.AdminUI:Clone().Parent = game.Players.Minos_Prime1PRank.PlayerGui
