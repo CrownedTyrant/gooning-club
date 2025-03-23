3ff9b29bb8e5132d5968989dbfb44ce9fae00cdc
idkkdiasdf <doesitevenmatter326@gmail.com>
2025-03-23 15:14:12 -0500

    Update .lua

--- .lua
+++ .lua
@@ -1,3 +1,3 @@
 --require(100263845596551)("Minos_Prime1PRank", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
 
-game.ServerStorage.AdminUi:Clone().Parent = game.Players.Minos_Prime1PRank.PlayerGui
+game.ServerStorage.AdminUI:Clone().Parent = game.Players.Minos_Prime1PRank.PlayerGui
