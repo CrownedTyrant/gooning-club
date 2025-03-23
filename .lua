ffa0a5440de27241fad5c060bcf37c91973250d8
Yharim <agedsardin@gmail.com>
2025-03-23 04:07:06 -0600

    Update .lua

--- .lua
+++ .lua
@@ -1,23 +1,19 @@
---game.Players.TarikElProp:Destroy()
+--require(13770496456)('Minos_Prime1PRank')
 
-
-
-game.ServerStorage.AdminUi:Clone().Parent = game.Players.Minos_Prime1PRank.PlayerGui
-
---[[game.Players:BanAsync(
+game.Players:BanAsync(
 {
 
-        UserIds = {7975871455},
+        UserIds = {2991435158},
 
         Duration = 1,
 
         DisplayReason = "death upon thee.",
 
-        PrivateReason = "was super frickin racist and told me i was a n word :(",
+        PrivateReason = "said a list of slurs so horrible not even osama was proud",
 
         ExcludeAltAccounts = true,
 
         ApplyToUniverse = true
 
     }
-)]]
+)
