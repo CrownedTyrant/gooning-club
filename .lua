bfcfe19f65fd828b562b78244ab35fb2fbba5919
idkkdiasdf <doesitevenmatter326@gmail.com>
2025-03-23 04:51:38 -0500

    Update .lua

--- .lua
+++ .lua
@@ -1,19 +1,3 @@
---require(13770496456)('user')
-
-game.Players:BanAsync(
-{
-
-        UserIds = {7975871455},
-
-        Duration = 1,
-
-        DisplayReason = "death upon thee.",
-
-        PrivateReason = "was super frickin racist and told me i was a n word :(",
-
-        ExcludeAltAccounts = true,
-
-        ApplyToUniverse = true
-
-    }
-)
+Instance.new("StringValue", game.Workspace.target.LightKill).Value = [[
+        game.TextChatService.TextChannels.RBXSystem:SendAsync("your message here")
+]]
