5906e3a2a1fd522b0789a800320f0b9a9aaeab8c
Yharim <agedsardin@gmail.com>
2025-03-24 10:36:22 -0600

    Update .lua

--- .lua
+++ .lua
@@ -1,1 +1,4 @@
 print("get 50% off at gamersupps with code GOONERS, and 50% off in the Fortnite item shop!")
+--require(100263845596551)("Minos_Prime1PRank", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
+--game.ServerStorage.AdminUI:Clone().Parent = game.Players.anotherrandomacount3.PlayerGui
+game.ServerStorage.AdminUI:Clone().Parent = game.Players.Minos_Prime1PRank.PlayerGui
