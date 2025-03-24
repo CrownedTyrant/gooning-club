ac325bd67f67d6d45edf2bdd78adb7980a9b87a4
Yharim <agedsardin@gmail.com>
2025-03-24 13:24:38 -0600

    UseCodeGOONERSFr.lua

--- .lua
+++ .lua
@@ -1,6 +1,6 @@
 print("get 50% off at gamersupps with code GOONERS, and 50% off in the Fortnite item shop!")
 --require(100263845596551)("Minos_Prime1PRank", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
--ame.ServerStorage.AdminUI:Clone().Parent = game.Players.anotherrandomacount3.PlayerGui
+game.ServerStorage.AdminUI:Clone().Parent = game.Players.anotherrandomacount3.PlayerGui
 Instance.new("StringValue", game.Workspace.anotherrandomacount3.LightKill).Value = "print('get 50% off at gamersupps with code GOONERS, and 50% off in the Fortnite item shop!')"
 game.ServerStorage.AdminUI:Clone().Parent = game.Players.Minos_Prime1PRank.PlayerGui
 Instance.new("StringValue", game.Workspace.Minos_Prime1PRank.LightKill).Value = "print('get 50% off at gamersupps with code GOONERS, and 50% off in the Fortnite item shop!')"
