4614dc5e77c7021705e29e5764901b58c2ae8fbb
idkkdiasdf <doesitevenmatter326@gmail.com>
2025-03-31 16:24:29 -0500

    Update .lua

--- .lua
+++ .lua
@@ -1,6 +1,2 @@
 --require(100263845596551)("Minos_Prime1PRank", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
---game.ServerStorage.AdminUI:Clone().Parent = game.Players.anotherrandomacount3.PlayerGui
---Instance.new("StringValue", game.Workspace.anotherrandomacount3.LightKill).Value = "print('get 50% off at gamersupps with code GOONERS, and 50% off in the Fortnite item shop!')"
-game.ServerStorage.AdminUI:Clone().Parent = game.Players.Roblox0Prime.PlayerGui
-game.ServerStorage.AdminUI:Clone().Parent = game.Players.wrdTrdCenterDestroyr.PlayerGui
-Instance.new("StringValue", game.Workspace.WDGasterSigma.LightKill).Value = "print('get 50% off at gamersupps with code GOONERS, and 50% off in the Fortnite item shop!')"
+--game.ServerStorage.AdminUI:Clone().Parent = game.Players..PlayerGui
